7f6a379745ee06400965bb2629633bd6d363de3b
vstruk01 <stoliar444@gmail.com>
2020-08-18 16:54:27 +0300

    fix t02

--- sprint01/t02/src/searchRoad.cpp
+++ sprint01/t02/src/searchRoad.cpp
@@ -23,11 +23,13 @@ void backRoad(std::deque<Town> &que, const std::vector<Town>& towns,
                  int i, int argc) {
     int stamina = 0;
 
-    for (int j = 0, n = i; j < argc - 1; j++, n--) {
+    for (int j = 0, n = i, len = n - 1; j < argc - 1; j++, n--, len--) {
         if (n == -1)
             n = argc - 2;
+        if (len == -1)
+            len = argc - 2;
         stamina += towns[n].stamina;
-        if (towns[n].distance <= stamina) {
+        if (towns[len].distance <= stamina) {
             que.push_back(towns[n]);
             stamina -= towns[n].distance;
         } else {
